4e19c0f39634eb9760277cf8c9ee8533e5e94b27
Andrea K Barreiro <abarreiro@smu.edu>
2024-09-11 16:50:39 -0500

    Added comments
    compute_PC_center, computre_PCs_waveforms

--- MainLinking/compute_PCs_waveforms.m
+++ MainLinking/compute_PCs_waveforms.m
@@ -1,8 +1,9 @@
 function [PC_struct] = compute_PCs_waveforms(tetrode, ...
                                              units_for_PCA, ...
                                              waves_cell)
+% compute_PCs_waveforms: 
 % ----------------------------------------------------------------------- %
-% On input:
+% INPUTS:
 %   tetrode         --> index of tetrode in data file. E.g., if you have 
 %                       tetrodes [1 2 5 17] saved & want 17, then 
 %                       tetrode = 4
@@ -10,10 +11,14 @@ function [PC_struct] = compute_PCs_waveforms(tetrode, ...
 %   waves_cell      --> cell array most likely provided by
 %                       'extract_waveforms()'
 %
-% On output:
+% OUTPUTS:
 %   PC_struct       --> PCA results concatenated into struct // see code
 %                       for fields
 % ----------------------------------------------------------------------- %
+%
+% Called by: cluster_trees_by_file
+% Calls: pca
+%
 
 chns    = tetrode .*4 - [3 2 1 0]; % channels on this tetrode
 idx_base = 0;
@@ -22,6 +27,7 @@ num_waves = sum( [waves_cell{:,2}] );
 num_time_points = size(waves_cell{1,1}, 2);
 tall_spikes = nan( num_waves, 4 * num_time_points );
 tall_labels = nan( num_waves, 1 );
+
 for unit_iter = 1 : numel( units_for_PCA )
     current_unit = units_for_PCA( unit_iter );
     current_chns = waves_cell{ unit_iter, 4 };
